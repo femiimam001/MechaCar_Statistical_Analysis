826b1c7a62e06a54e9f9442f7341e8621f4dfb99
femiimam001 <olufemi.imam@gmail.com>
2022-03-31 09:51:15 -0400

    adding update

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -15,6 +15,10 @@ model <- lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+A
 names(Machacar)
 summary(model)
 
+6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,Machacar )) 
+summary(model)
+
 suscoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 suscoil
 summary(suscoil$PSI)
