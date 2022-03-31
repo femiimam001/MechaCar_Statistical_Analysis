52ada64e6b60be6393937fa520828c9a42bb5a68
femiimam001 <olufemi.imam@gmail.com>
2022-03-30 23:29:22 -0400

    adding updates

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,9 +1,17 @@
+#DELIVERABLE 1
 
+# 3. Use the library() function to load the dplyr package
+library(dplyr)\
+
+#4. Import and read in the MechaCar_mpg.csv file as a dataframe.
+library(tidyverse)
 carData <- read.csv("C:/OlufemiProjectWork/AnalysisProject/Statistics&RChallenge/MechaCar_Statistical_Analysis/Resources") 
 Machacar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 View(Machacar)
-library(dplyr)
-model <- lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,Machacar) #create linear model
+#library(dplyr)
+
+#5. Perform linear regression using the lm() function
+model <- lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,Machacar) 
 names(Machacar)
 summary(model)
 
